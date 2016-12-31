60612cc1a6b02a0fdb5ba3d22299a16303345cda
jttwnsnd <jt.a.townsend@gmail.com>
2016-12-31 13:06:52 -0500

    building youtube clone list

--- Projects/Project1/src/index.js
+++ Projects/Project1/src/index.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
+//Components Called
 import SearchBar from './components/search-bar';
+import VideoList from "./components/video-list"
 
+//My Api key for this project. will be deleted once project is done, so be sure to create a new one from google.
 const API_KEY = 'AIzaSyAa6eCwuRMYNI4VAXLW5qKNAyKL6Di6r_4';
 //Create a new component. This component should produce
 // some html
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      //only works when key and value pair are same name.
+      this.setState({ videos })
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 //Take this component's generated HTML and
