cba52302cd638f51b09faeb6fc1c0ad49b25e140
jttwnsnd <jt.a.townsend@gmail.com>
2017-01-04 11:56:56 -0500

    building the query string

--- Projects/Project3/src/actions/index.js
+++ Projects/Project3/src/actions/index.js
@@ -1,1 +1,14 @@
 //Sign up for a free api key at openweathermap.org
+const API_KEY = 
+
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  return {
+    type: FETCH_WEATHER
+  }
+
+}
