fdbb6632249d7a60160ebdfdfc9f61e7e60f6773
jttwnsnd <jt.a.townsend@gmail.com>
2017-01-04 13:49:52 -0500

    building reducer for api request

--- Projects/Project3/src/actions/index.js
+++ Projects/Project3/src/actions/index.js
@@ -1,16 +1,18 @@
 import axios from 'axios';
 
 //Sign up for a free api key at openweathermap.org
-const API_KEY = '
+const API_KEY = 
 
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export fetchWeather(city) {
+export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   }
 
 }
